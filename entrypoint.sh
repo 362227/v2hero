48e3adea100b26aa1e6b427139b88c7d92e325c5
362227 <91358231+362227@users.noreply.github.com>
2022-12-20 18:04:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ echo -e -n "$PORT" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
+curl -L https://raw.githubusercontent.com/362227/Xray-heroku/main/tcp.config.json > config.json
+wget --no-check-certificate https://raw.githubusercontent.com/362227/Xray-heroku/main/tcp.config.json -O config.json
 
 ./v2ray
